eb774d166babbdb02ca9d44846b53839648d6226
zach-brown <z.brown181818@protonmail.com>
2020-11-23 17:48:40 -0800

    Added get_binary_columns() to eda/df_formatting

--- eda/df_formatting.py
+++ eda/df_formatting.py
@@ -13,3 +13,22 @@ def format_column_titles(df):
     
 def get_str_column_names(df):
     return [col for col in df.columns if type(df[col][0]) is str]  
+
+def isnan(x):
+    if x != x:
+        return True
+    return False
+
+def get_binary_columns(df):
+    '''Returns a list of column names for only binary columns'''
+    cols = []
+    for col in df:
+        binary = len(df[col].unique()) == 2
+        three_values = len(df[col].unique()) == 3
+        has_nan = not all(isnan(name) for name in df[col].unique())
+        if binary:
+            cols.append(col)
+        if three_values & has_nan:
+            cols.append(col)
+    
+    return cols
