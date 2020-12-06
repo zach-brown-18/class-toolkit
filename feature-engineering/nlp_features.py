ac0dc2f83eee3b592a4511dd1d6e30a7bf330be8
zach-brown <z.brown181818@protonmail.com>
2020-12-06 11:01:57 -0800

    nlp_features module add_sentiment_columns() added column to be transformed.

--- feature-engineering/nlp_features.py
+++ feature-engineering/nlp_features.py
@@ -1,8 +1,8 @@
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
-def add_sentiment_columns(df):
+def add_sentiment_columns(df, text_col):
     sentiment = SentimentIntensityAnalyzer()
-    df['sentiment'] = df['all_text'].map(sentiment.polarity_scores)
+    df['sentiment'] = df[text_col].map(sentiment.polarity_scores)
     df['neg_sent'] = df['sentiment'].map(lambda x: x['neg'])
     df['neu_sent'] = df['sentiment'].map(lambda x: x['neu'])
     df['pos_sent'] = df['sentiment'].map(lambda x: x['pos'])
