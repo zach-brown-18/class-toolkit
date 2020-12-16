1525f1b4b122e3a6ced94bfa574236a92cc38820
zach-brown <z.brown181818@protonmail.com>
2020-12-16 15:32:52 -0800

    Updated strip_white to remove multiple spaces

--- eda/cleaning.py
+++ eda/cleaning.py
@@ -1,8 +1,12 @@
 import columns
 import pandas as pd
+import re
 
 def strip_white(df):
-    '''Strip leading and trailing whitespace for all string columns'''
+    '''Strip multiple spaces, including tabs, new lines, and leading and trailing whitespace for all string columns'''
     string_columns = columns.get_str_column_names(df)
     for col in string_columns:
         df[col] = df[col].map(str.strip)
+        df[col] = df[col].map(lambda x: re.sub('\s+',' ', x))
+
+
