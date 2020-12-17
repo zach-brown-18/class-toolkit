4605a467a4d0a61c426ffb4c61c01b4e29f8842c
zach-brown <z.brown181818@protonmail.com>
2020-12-16 21:09:37 -0800

    Removed columns.py import from cleaning.py

--- eda/cleaning.py
+++ eda/cleaning.py
@@ -1,10 +1,9 @@
-import columns
 import pandas as pd
 import re
 
 def strip_white(df):
     '''Strip multiple spaces, including tabs, new lines, and leading and trailing whitespace for all string columns'''
-    string_columns = columns.get_str_column_names(df)
+    string_columns = [col for col in df.columns if type(df[col][0]) is str]
     for col in string_columns:
         df[col] = df[col].map(str.strip)
         df[col] = df[col].map(lambda x: re.sub('\s+',' ', x))
